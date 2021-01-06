4cdbab50eada4c2a00281b35b7e36232add092a6
Sidney Alceu Andrade Wanderley Junior <sidney.andrade@gmail.com>
2021-01-06 11:58:08 -0300

    Criado metodos

--- RemoteHOST/src/remoteHOST/BancoDeDados.java
+++ RemoteHOST/src/remoteHOST/BancoDeDados.java
@@ -4,5 +4,26 @@ public class BancoDeDados {
 	private String UsuarioDoBanco;
 	private String SenhaDoBanco;
 	private String NomeDoBanco;
+	
+	public String getUsuarioDoBanco() {
+		return UsuarioDoBanco;
+	}
+	public void setUsuarioDoBanco(String usuarioDoBanco) {
+		UsuarioDoBanco = usuarioDoBanco;
+	}
+	public String getSenhaDoBanco() {
+		return SenhaDoBanco;
+	}
+	public void setSenhaDoBanco(String senhaDoBanco) {
+		SenhaDoBanco = senhaDoBanco;
+	}
+	public String getNomeDoBanco() {
+		return NomeDoBanco;
+	}
+	public void setNomeDoBanco(String nomeDoBanco) {
+		NomeDoBanco = nomeDoBanco;
+	}
+	
+	
 
 }
--- RemoteHOST/src/remoteHOST/Host.java
+++ RemoteHOST/src/remoteHOST/Host.java
@@ -4,6 +4,24 @@ public class Host {
 	private String Ip;
 	private String NomeDoHost;
 	private String TipodeConexao;
+	public String getIp() {
+		return Ip;
+	}
+	public void setIp(String ip) {
+		Ip = ip;
+	}
+	public String getNomeDoHost() {
+		return NomeDoHost;
+	}
+	public void setNomeDoHost(String nomeDoHost) {
+		NomeDoHost = nomeDoHost;
+	}
+	public String getTipodeConexao() {
+		return TipodeConexao;
+	}
+	public void setTipodeConexao(String tipodeConexao) {
+		TipodeConexao = tipodeConexao;
+	}
 	
 	
 
