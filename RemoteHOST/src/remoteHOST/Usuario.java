6bb6761263338358ba9f727a754cb6a7b4ebf598
Sidney Alceu Andrade Wanderley Junior <sidney.andrade@gmail.com>
2021-01-07 15:22:59 -0300

    banco de dados

---
+++ RemoteHOST/src/remoteHOST/Usuario.java
@@ -0,0 +1,35 @@
+package remoteHOST;
+
+public class Usuario {
+	private String IdUsuario;
+	private String Senha;
+	private String Login;
+	private String Nome;
+	
+	public String getIdUsuario() {
+		return IdUsuario;
+	}
+	public void setIdUsuario(String idUsuario) {
+		IdUsuario = idUsuario;
+	}
+	public String getSenha() {
+		return Senha;
+	}
+	public void setSenha(String senha) {
+		Senha = senha;
+	}
+	public String getLogin() {
+		return Login;
+	}
+	public void setLogin(String login) {
+		Login = login;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	
+	
+}
